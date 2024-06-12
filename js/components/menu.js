716dbf19d1b75e0be10844a03d72ac378b630a9f
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-06-12 13:41:35 -0500

    feat: :sparkles: Archivos con su respectiva direccion

---
+++ js/components/menu.js
@@ -0,0 +1,15 @@
+export const menulistCategoryIndex = (res)=>{
+    let {data} = res;
+    let plantilla = "";
+    data.forEach((value, index) => {
+        plantilla += /*html*/`
+        <li title="${value.name}">
+            <a href="#">
+                <img src="storage/img/categoryHover.svg">
+                <span> ${value.name}</span>
+            </a>
+        </li>
+        `;
+    });
+    return plantilla;
+} 
