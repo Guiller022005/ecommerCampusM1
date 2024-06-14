2b919686711a8f11f83e4c16b8c12186371f16f7
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-06-14 14:28:26 -0500

    feat: :sparkles: Informacion del detalle del producto
    La seccion del titulo del producto fue terminada

--- js/components/detail.js
+++ js/components/detail.js
@@ -1,14 +1,18 @@
 import { getProductId } from "../module/detail.js";
+import { titleProductDetail } from "./section.js";
 import { galleryCategory } from "./gallery.js";
 
 
 let main__section_gallery = document.querySelector("#main__section_gallery");
+let main__section__title = document.querySelector("#main__section__title");
 
 addEventListener("DOMContentLoaded", async(e)=>{
     let params = new URLSearchParams(location.search);
     let id = params.get('id');
     if(!localStorage.getItem(id)) localStorage.setItem(id, JSON.stringify(await getProductId({id})));
-    main__section_gallery.innerHTML = await galleryCategory(JSON.parse(localStorage.getItem(id)))
+    let info = JSON.parse(localStorage.getItem(id));
+    main__section_gallery.innerHTML = await galleryCategory(info);
+    main__section__title.innerHTML = await titleProductDetail(info);
     // let {data} = res;
     // let {
     //     category_path,
