3926cdca56b3e58330953f84e17446d0e35682d0
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-06-21 08:12:21 -0500

    feat: :sparkles: Agrego img y nombre, cambio de direccion de api

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': '19b4d5cb79mshc23426d0f4c4459p12c7d2jsn4af21ef974c9',
+    'x-rapidapi-key': 'c769f008c6mshfede8b003d09864p10f9f5jsn13ebab54dc34',
 	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
