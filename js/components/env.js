61937f876f3c66214c1f2363c7c8e8772f0f319a
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-06-24 15:05:02 -0500

    feat: :sparkles: Personalizacion de color de pag

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': 'c769f008c6mshfede8b003d09864p10f9f5jsn13ebab54dc34',
+    'x-rapidapi-key': 'a564e7609cmshb120cb112357053p13a707jsne7c55b8b65f7',
 	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
